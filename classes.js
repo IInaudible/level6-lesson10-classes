20865a74fc5eee591309f7eaa9539367f5d2e20c
michaelberish <mikeyberish@gmail.com>
2022-05-27 14:04:44 -0700

    done

--- classes.js
+++ classes.js
@@ -1,18 +1,44 @@
-// Step 1: Create a base class called ZooAnimal that can store the animal name, animal type, and adoption date. It can also print these details to the console when needed through a function named "printDetails".
+class ZooAnimal {
+    static adoptions = 0;
+    constructor (animalName, animalType, date) {
+        this.animalName = animalName;
+        this.animalType = animalType;
+        this.date = date;
+        ZooAnimal.adoptions++;
+    }
 
+    printDetails() {
+        console.log(this.animalName, this.animalType, this.date);
+    }
 
+    static printAmount() {
+        console.log(this.adoptions, 'Animals')
+    }
+}
 
-// Step 2: Create extended classes off ZooAnimal called ZooBird and ZooMammal that can automatically store the animal type without passing it in. You will need to create constructors for ZooBird and ZooMammal that only take the animal name and adoption date, and then use the "super" keyword inside them to utilize the ZooAnimal constructor to store the data.
+class ZooBird extends ZooAnimal {
+    static birdAdoptions = 0;
+    constructor (animalName, date) {
+        super (animalName, 'Bird', date);
+        ZooBird.birdAdoptions++;
+    }
 
+    static printAmount() {
+        console.log(this.birdAdoptions, 'Birds')
+    }
+}
 
+class ZooMammal extends ZooAnimal {
+    static mammalAdoptions = 0;
+    constructor (animalName, date) {
+        super (animalName, 'Mammal', date);
+        ZooMammal.mammalAdoptions++;
+    }
 
-// Step 3: Modify ZooAnimal to use a static variable in its constructor to track how many Zoo Animals have been adopted, and update that variable any time a new Zoo Animal is created. Also use a static method to add the ability to print this info to the console when needed through a function named "printAmount". Because the "super" keyword was used earlier to utilize the ZooAnimal constructor, the static variable will be updated automatically.
-
-
-
-// Step 4: Modify ZooBird and ZooMammal to similarly track how many birds and mammals have been adopted using static variables in their constructors, and add the ability to print this info to the console when needed
-
-
+    static printAmount() {
+        console.log(this.mammalAdoptions, 'Mammals')
+    }
+}
 
 // Step 5: Run the code with Node.js, and note that the console should print out the following:
 // Crocodile / Reptile / Jan 22, 2022
@@ -22,10 +48,6 @@
 // 1 birds
 // 1 mammals
 
-// Step 6: If the above is printed out correctly, you're done. If the above is not printed to the console correctly, debug your code to find out how to resolve that.
-
-
-// Review the code below but don't remove, modify, or add to it. It is used to create zoo animals from your above code and print details to the console
 let crocodile = new ZooAnimal("Crocodile", "Reptile", "Jan 22, 2022");
 let flamingo = new ZooBird("Flamingo", "Feb 20, 2022");
 let tiger = new ZooMammal("Tiger", "Feb 12, 2022");
